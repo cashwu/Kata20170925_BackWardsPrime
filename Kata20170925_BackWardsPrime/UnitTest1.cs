073ef5a06bd123060280966dadda6aafe7c1a649
cash <cash@cashwu.com>
2017-09-25 21:10:01 +0800

    Add project files.

---
+++ Kata20170925_BackWardsPrime/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170925_BackWardsPrime
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
