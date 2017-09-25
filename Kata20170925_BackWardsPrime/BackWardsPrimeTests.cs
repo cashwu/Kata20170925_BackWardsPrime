49db6b70d9b34e90501bd08a3251b54a66fe5601
cash <cash@cashwu.com>
2017-09-25 21:13:52 +0800

    init

--- Kata20170925_BackWardsPrime/BackWardsPrimeTests.cs
+++ Kata20170925_BackWardsPrime/BackWardsPrimeTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170925_BackWardsPrime
 {
     [TestClass]
-    public class UnitTest1
+    public class BackWardsPrimeTests
     {
         [TestMethod]
         public void TestMethod1()
